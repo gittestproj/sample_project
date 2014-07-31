63d997735a091e060e1aca5aac3b7e0fbc462114
root <root@epbyminw3088t1.minsk.epam.com>
2014-07-31 21:59:16 +0300

    some changes made

--- fact.c
+++ fact.c
@@ -1,8 +1,13 @@
 int main()
 {
-int n,k = 1;
+unsigned int n,k = 1;
 scanf("%i",&n);
 int i;
+if (n > 20) {
+ printf("Too high number!");
+ return 1;
+}
 for (i=1; i < n; i++) k *= i;
 printf("N! = %i",k);
+return 0;
 }
