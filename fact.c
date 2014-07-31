6059d685f7a4a2a8f8e1b8fbbc1c87fadc289632
root <root@epbyminw3088t1.minsk.epam.com>
2014-08-01 00:14:00 +0300

    fixed

--- fact.c
+++ fact.c
@@ -6,10 +6,10 @@ printf("Enter N: ");
 scanf("%i",&n);
 int i;
 if (n > 20) {
- printf("Too high number!");
+ printf("Invalid number!");
  return 1;
 }
-for (i=1; i < n; i++) k *= i;
+for (i=1; i < (n+1); i++) k *= i;
 printf("N! = %i",k);
 return 0;
 }
